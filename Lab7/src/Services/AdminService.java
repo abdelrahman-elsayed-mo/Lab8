7db0b8cdb167a4ac89258ddbe7005246319324e0
Youssef Fathy <yousseffathy771@gmail.com>
2025-11-22 22:06:10 +0200

    added lines for the AdminDashboard

--- Lab7/src/Services/AdminService.java
+++ Lab7/src/Services/AdminService.java
@@ -54,4 +54,16 @@ public class AdminService {
         }
         return false;
     }
+
+    public boolean approve(String courseId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public boolean reject(String courseId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public List<Course> getPending() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
