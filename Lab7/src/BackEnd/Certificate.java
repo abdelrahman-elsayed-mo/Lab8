70e1ced84e3898014374234e447a68c57697827a
AdamAyman <adamaymna7812@gmail.com>
2025-11-23 09:46:47 +0200

    Merge origin/main into main
    # Conflicts:
    #	lab4/src/lab4/Lab4.java

--- Lab7/src/BackEnd/Certificate.java
+++ Lab7/src/BackEnd/Certificate.java
@@ -1,10 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package BackEnd;
+
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 
 public class Certificate {
+    
+    private final String certificateID;
+    private String studentID;
+    private String courseID;
+    private LocalDate issueDate;
+    
+    public Certificate(String sID , String cID)
+    {
+        this.certificateID="SKILL FORGE CERT-"+UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+        this.courseID= cID;
+        this.studentID= sID;
+        this.issueDate= LocalDate.now();    
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getCertificateID() {
+        return certificateID;
+    }
+  
     
 }
